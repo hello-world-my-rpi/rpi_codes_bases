21b99efe7efd1c7049fbb8089057eaa78df4bf6f
yunue <1934182494@qq.com>
2019-09-22 16:18:41 +0800

    Update get_news.py

--- news/get_news.py
+++ news/get_news.py
@@ -1,25 +1,22 @@
 from spider_news import get_url
-import re,time,random
+import re
 
 
+def get_news():
 html_home = get_url('home')
 html_world = get_url('world')
 
 if html_home or html_world:
-    pattern_home = '''<li><a href="https://news.sina.com.cn/.*?>(.*?)</a></li>'''
-    pattern_world = '''<h2><a href="http.*?://news.sina.com.cn/.*?target="_blank".*?>(.*?)</a></h2>'''
+        pattern_home = '''<li><a href="https://news.sina.com.cn/.*?>(.*?)</a></li>'''
+        pattern_world = '''<h2><a href="http.*?://news.sina.com.cn/.*?target="_blank".*?>(.*?)</a></h2>'''
                        
       
-    pt_h = re.compile(pattern_home)
-    pt_w = re.compile(pattern_world)
+        pt_h = re.compile(pattern_home)
+        pt_w = re.compile(pattern_world)
 
-    result_home = re.findall(pt_h,html_home)
-    result_world = re.findall(pt_w,html_world)
-    print(result_world)    result = result_home + result_world
+        result_home = re.findall(pt_h,html_home)
+        result_world = re.findall(pt_w,html_world)
+        result = result_home + result_world
 
-    while True:
-        if input('cmd:')=='n':
-            print(random.choice(result))
-        else:
-            break
+        return result
         
